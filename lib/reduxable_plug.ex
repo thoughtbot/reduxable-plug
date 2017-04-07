8152c96b4deb084bf88a6d4d288f051aae7aced9
Paul Smith <paulcsmith0218@gmail.com>
2017-04-07 11:07:39 -0400

    Initial commit

---
+++ lib/reduxable_plug.ex
@@ -0,0 +1,18 @@
+defmodule ReduxablePlug do
+  @moduledoc """
+  Documentation for ReduxablePlug.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReduxablePlug.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
